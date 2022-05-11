62aedce5794b5a4e9052919667ffc40860390925
Carl Uebelhart <carl@erutan.net>
2022-05-11 14:07:54 -0700

    initial scatterfieldplus

---
+++ src/Plugin/views/field/ScatterFieldPlus.php
@@ -0,0 +1,207 @@
+<?php
+
+namespace Drupal\charts\Plugin\views\field;
+
+use Drupal\Component\Serialization\Json;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Messenger\MessengerInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\views\Plugin\views\field\FieldPluginBase;
+use Drupal\views\ResultRow;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * @file
+ * Defines Drupal\charts\Plugin\views\field\ScatterFieldPlus.
+ */
+
+/**
+ * Field handler to provide x and y values for a scatter chart, plus a bonus field to be used elsewhere.
+ *
+ * @ingroup views_field_handlers
+ * @ViewsField("field_charts_fields_scatter_plus")
+ */
+class ScatterFieldPlus extends FieldPluginBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The messenger service.
+   *
+   * @var \Drupal\Core\Messenger\MessengerInterface
+   */
+  protected $messenger;
+
+  /**
+   * Constructs a \Drupal\views\Plugin\Block\ViewsBlockBase object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Messenger\MessengerInterface $messenger
+   *   The messenger.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, MessengerInterface $messenger) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->messenger = $messenger;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('messenger')
+    );
+  }
+
+  /**
+   * Sets the initial field data at zero.
+   */
+  public function query() {
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function defineOptions() {
+    $options = parent::defineOptions();
+    $this->field_alias = 'scatter_plus_field';
+    $options['fieldset_one']['default'] = NULL;
+    $options['fieldset_two']['default'] = NULL;
+    $options['fieldset_three']['default'] = NULL;
+    $options['fieldset_one']['x_axis'] = ['default' => NULL];
+    $options['fieldset_two']['y_axis'] = ['default' => NULL];
+    $options['fieldset_three']['bonus'] = ['default' => NULL];
+
+    return $options;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildOptionsForm(&$form, FormStateInterface $form_state) {
+    parent::buildOptionsForm($form, $form_state);
+    $fieldList = $this->displayHandler->getFieldLabels();
+    unset($fieldList['field_charts_fields_scatter_plus']);
+
+    $form['fieldset_one'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('Select the field representing the X axis.'),
+      '#collapsible' => FALSE,
+      '#collapsed' => FALSE,
+      '#weight' => -10,
+      '#required' => TRUE,
+    ];
+    $form['fieldset_one']['x_axis'] = [
+      '#type' => 'radios',
+      '#title' => $this->t('X Axis Field'),
+      '#options' => $fieldList,
+      '#default_value' => $this->options['fieldset_one']['x_axis'],
+      '#weight' => -10,
+    ];
+
+    $form['fieldset_two'] = [
+      '#type' => 'fieldset',
+      '#collapsible' => FALSE,
+      '#collapsed' => FALSE,
+      '#title' => $this->t('Select the field representing the Y axis.'),
+      '#weight' => -9,
+      '#required' => TRUE,
+    ];
+    $form['fieldset_two']['y_axis'] = [
+      '#type' => 'radios',
+      '#title' => $this->t('Y Axis Field'),
+      '#options' => $fieldList,
+      '#default_value' => $this->options['fieldset_two']['y_axis'],
+      '#weight' => -9,
+    ];
+
+    $form['fieldset_three'] = [
+      '#type' => 'fieldset',
+      '#collapsible' => FALSE,
+      '#collapsed' => FALSE,
+      '#title' => $this->t('Select the field to be returned as a value with the key of "bonus".'),
+      '#weight' => -9,
+      '#required' => TRUE,
+    ];
+    $form['fieldset_three']['bonus'] = [
+      '#type' => 'radios',
+      '#title' => $this->t('Bonus Field'),
+      '#options' => $fieldList,
+      '#default_value' => $this->options['fieldset_three']['bonus'],
+      '#weight' => -9,
+    ];
+
+    return $form;
+  }
+
+  /**
+   * Get the value of a simple math field.
+   *
+   * @param \Drupal\views\ResultRow $values
+   *   Row results.
+   * @param string $fieldset
+   *   The items fieldset.
+   * @param string $axis
+   *   Whether we are fetching field one's value.
+   *
+   * @return mixed
+   *   The field value.
+   *
+   * @throws \Exception
+   */
+  protected function getFieldValue(ResultRow $values, $fieldset, $axis) {
+
+    $field = $this->options[$fieldset][$axis];
+
+    $data = NULL;
+
+    // Fetch the data from the database alias.
+    if (isset($this->view->field[$field])) {
+      if ($field == 'scatter_plus_field') {
+        $data = $this->view->field['field_charts_fields_scatter_plus']->getValue($values);
+      }
+      else {
+        $data = $this->view->field[$field]->getValue($values);
+      }
+    }
+
+    if (!isset($data)) {
+      // There's no value. Default to NULL.
+      $data = NULL;
+    }
+
+    // Ensure the input is numeric.
+    if (!is_numeric($data)) {
+      $this->messenger->addError($this->t('Check the formatting of your
+        scatter_plus Field inputs: one or both of them are not numeric.'));
+    }
+
+    return $data;
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * @throws \Exception
+   */
+  public function getValue(ResultRow $values, $field = NULL) {
+    parent::getValue($values, $field);
+
+    $xAxisFieldValue = $this->getFieldValue($values, 'fieldset_one', 'x_axis');
+    $yAxisFieldValue = $this->getFieldValue($values, 'fieldset_two', 'y_axis');
+    $bonusFieldValue = $this->getFieldValue($values, 'fieldset_three', 'bonus');
+
+    return Json::encode([
+      Json::decode($xAxisFieldValue),
+      Json::decode($yAxisFieldValue),
+      Json::decode($bonusFieldValue),
+    ]);
+  }
+
+}
